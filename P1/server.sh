21e327d4de6ccbceff2ffec16b9ef7ae61144254
hfanzaou <xboy_hatim@hotmail.fr>
2025-04-09 21:26:24 +0100

    P2 almost done

--- P1/server.sh
+++ P1/server.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt install -y curl
 
 # Install K3s in server mode
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644" INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip=192.168.56.110" sh -
 
 # copy the node-token to shared folder
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
