c6b58d83bf6d96be7d97050d5d194d0c6d482c1c
EBensalt <ebensalt@student.1337.ma>
2025-04-19 19:52:00 +0100

    update in the install.sh script

--- P3/scripts/install.sh
+++ P3/scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -10,8 +10,9 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt install curl -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER
+newgrp docker
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
