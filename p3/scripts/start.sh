7c8f13a93f54677f9b48e655c4bc14b86108e3de
EBensalt <ebensalt@student.1337.ma>
2025-04-22 02:06:43 +0100

    now its done

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -32,8 +32,4 @@ while true; do
 	kubectl get pods -n dev | grep -v NAME | awk '{print $2}' | grep -v -E '1/1' > /dev/null || break
 	sleep 1
 done
-cd confs/app
-git init
-git remote add origin https://github.com/EBensalt/hfanzaou.git
-git branch -m master main
 echo -e "${GREEN}✅ All done! Argo CD and dev app are up and running.${NC}"
